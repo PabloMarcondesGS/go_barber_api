15d335e63079209615e6692cf0efd2d1dbbf41ba
pablo <pablo@radius.net.br>
2020-11-10 21:43:49 -0300

    adicionando templete ao envio de email

--- src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
-import IParseMailTemplateDTO from '../dtos/IParseMailTempleteProviderDTO';
 import IMailTemplateProvider from '../models/IMailTemplateProvider';
 
 class FakeMailTemplatePRovider implements IMailTemplateProvider {
-    public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
-        return template;
+    public async parse(): Promise<string> {
+        return 'Mail content';
     }
 }
 
